5c0e5504e6a7ced5caeddfd2666a35e5c7388835
p1210292 <p1210292@IUTAINFOPC168.univ-lyon1.fr>
2014-02-24 11:46:03 +0100

    5

--- src/applisynthese/accesBDInfo.java
+++ src/applisynthese/accesBDInfo.java
@@ -3,11 +3,33 @@
  * and open the template in the editor.
  */
 package applisynthese;
-
+import connexion.ConnexionOracleFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import métier.Employe;
 /**
  *
  * @author p1210292
  */
 public class accesBDInfo {
-    
+    private Connection connexionBD;
+    // Constructeur privé pour le singleton.
+    private accesBDInfo() {
+    connexionBD = ConnexionOracleFactory.creerConnexion();
+    // initialiser les PreparedStatement();
+    }
+
+// L'unique instance de la classe
+private static accesBDInfo instance = null;
+/**
+* Cette fonction retourne l'unique instance de la classe
+*/
+public static accesBDInfo getInstance() {
+if (instance == null)
+instance = new accesBDInfo();
+return instance;
 }
+
+}
+
